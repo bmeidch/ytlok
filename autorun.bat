feaa4cb0f0cb867aa9c076121339f31ada4d14b7
bmeidch <86718947+bmeidch@users.noreply.github.com>
2022-12-09 12:43:39 +0700

    Up

--- autorun.bat
+++
@@ -1,4 +0,0 @@
-pip install requests
-
-cd scripts/
-python babybus_m3ugrabber.py > ../babybus.m3u
---
+++ babybus.bat
@@ -0,0 +1,4 @@
+pip install requests
+
+cd scripts/
+python babybus_m3ugrabber.py > ../babybus.m3u
